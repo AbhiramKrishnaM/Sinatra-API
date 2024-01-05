af3db3b8a0e5f3d489967f68047cbff344ca3341
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-05 14:33:58 +0530

    serving html files

--- server.rb
+++ server.rb
@@ -8,3 +8,12 @@ get "/get-hosts/:id" do
   content_type :json
   { :param => params['id'], :query => params['query'] }.to_json
 end
+
+get "/hello/:name" do
+  puts "#{params['name']}"
+end
+
+
+get "/erb" do
+  erb :index
+end
